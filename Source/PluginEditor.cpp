e08af3986120239161313e6ff959cd2f0b795929
kashiwade <music5d7_9265@outlook.jp>
2024-08-20 22:15:16 +0900

    UIの作成(JUCE準備編)

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,7 +16,11 @@ GainPanTutorialAudioProcessorEditor::GainPanTutorialAudioProcessorEditor(
     : AudioProcessorEditor(&p), audioProcessor(p) {
   // Make sure that before the constructor has finished, you've set the
   // editor's size to whatever you need it to be.
-  setSize(400, 300);
+
+  addAndMakeVisible(webComponent);
+  webComponent.goToURL("http://localhost:5173/");
+  // webComponent.goToURL(juce::WebBrowserComponent::getResourceProviderRoot());
+  setSize(170, 650);
 }
 
 GainPanTutorialAudioProcessorEditor::~GainPanTutorialAudioProcessorEditor() {}
@@ -27,14 +31,61 @@ void GainPanTutorialAudioProcessorEditor::paint(juce::Graphics& g) {
   // solid colour)
   g.fillAll(
       getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-
-  g.setColour(juce::Colours::white);
-  g.setFont(juce::FontOptions(15.0f));
-  g.drawFittedText("Hello World!", getLocalBounds(),
-                   juce::Justification::centred, 1);
 }
 
 void GainPanTutorialAudioProcessorEditor::resized() {
   // This is generally where you'll want to lay out the positions of any
   // subcomponents in your editor..
+  webComponent.setBounds(getLocalBounds());
+}
+
+std::optional<juce::WebBrowserComponent::Resource>
+GainPanTutorialAudioProcessorEditor::getResource(const juce::String& url) {
+  const auto urlToRetrive = url == "/"
+                                ? juce::String{"index.html"}
+                                : url.fromFirstOccurrenceOf("/", false, false);
+
+  static auto streamZip = juce::MemoryInputStream(
+      juce::MemoryBlock(BinaryData::assets_zip, BinaryData::assets_zipSize),
+      true);
+
+  static juce::ZipFile archive{streamZip};
+
+  if (auto* entry = archive.getEntry(urlToRetrive)) {
+    auto entryStream = rawToUniquePtr(archive.createStreamForEntry(*entry));
+    std::vector<std::byte> result((size_t)entryStream->getTotalLength());
+    entryStream->setPosition(0);
+    entryStream->read(result.data(), result.size());
+
+    auto mime = getMimeForExtension(
+        entry->filename.fromLastOccurrenceOf(".", false, false).toLowerCase());
+    return juce::WebBrowserComponent::Resource{std::move(result),
+                                               std::move(mime)};
+  }
+  return std::nullopt;
+}
+
+const char* GainPanTutorialAudioProcessorEditor::getMimeForExtension(
+    const juce::String& extension) {
+  static const std::unordered_map<juce::String, const char*> mimeMap = {
+      {{"htm"}, "text/html"},
+      {{"html"}, "text/html"},
+      {{"txt"}, "text/plain"},
+      {{"jpg"}, "image/jpeg"},
+      {{"jpeg"}, "image/jpeg"},
+      {{"svg"}, "image/svg+xml"},
+      {{"ico"}, "image/vnd.microsoft.icon"},
+      {{"json"}, "application/json"},
+      {{"png"}, "image/png"},
+      {{"css"}, "text/css"},
+      {{"map"}, "application/json"},
+      {{"js"}, "text/javascript"},
+      {{"woff2"}, "font/woff2"}};
+
+  if (const auto it = mimeMap.find(extension.toLowerCase());
+      it != mimeMap.end())
+    return it->second;
+
+  jassertfalse;
+  return "";
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -12,6 +12,16 @@
 
 #include "PluginProcessor.h"
 
+struct SinglePageBrowser : juce::WebBrowserComponent {
+  using WebBrowserComponent::WebBrowserComponent;
+
+  // Prevent page loads from navigating away from our single page web app
+  bool pageAboutToLoad(const juce::String& newURL) override {
+    return newURL == juce::String("http://localhost:5173/") ||
+           newURL == getResourceProviderRoot();
+  }
+};
+
 //==============================================================================
 /**
  */
@@ -23,12 +33,61 @@ class GainPanTutorialAudioProcessorEditor : public juce::AudioProcessorEditor {
   //==============================================================================
   void paint(juce::Graphics&) override;
   void resized() override;
+  //==============================================================================
+  int getControlParameterIndex(juce::Component&) override {
+    return controlParameterIndexReceiver.getControlParameterIndex();
+  }
 
  private:
   // This reference is provided as a quick way for your editor to
   // access the processor object that created it.
   GainPanTutorialAudioProcessor& audioProcessor;
 
+  //==============================================================================
+
+  juce::WebControlParameterIndexReceiver controlParameterIndexReceiver;
+
+  juce::WebSliderRelay gainRelay{webComponent, "gain"};
+  juce::WebSliderRelay panRelay{webComponent, "panAngle"};
+  juce::WebComboBoxRelay panRuleRelay{webComponent, "panRule"};
+  juce::WebToggleButtonRelay bypassRelay{webComponent, "bypass"};
+
+  //==============================================================================
+
+  SinglePageBrowser webComponent{
+      juce::WebBrowserComponent::Options{}
+          .withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
+          .withWinWebView2Options(
+              juce::WebBrowserComponent::Options::WinWebView2{}
+                  .withUserDataFolder(juce::File::getSpecialLocation(
+                      juce::File::SpecialLocationType::tempDirectory)))
+          .withOptionsFrom(gainRelay)
+          .withOptionsFrom(panRelay)
+          .withOptionsFrom(panRuleRelay)
+          .withOptionsFrom(bypassRelay)
+          .withOptionsFrom(controlParameterIndexReceiver)
+          .withResourceProvider(
+              [this](const auto& url) { return getResource(url); },
+              juce::URL{"http://localhost:5173/"}.getOrigin())};
+
+  std::optional<juce::WebBrowserComponent::Resource> getResource(
+      const juce::String& url);
+
+  const char* getMimeForExtension(const juce::String& extension);
+
+  //==============================================================================
+
+  juce::WebSliderParameterAttachment gainAttachment{
+      *audioProcessor.parameters.getParameter("gain"), gainRelay, nullptr};
+  juce::WebSliderParameterAttachment panAttachment{
+      *audioProcessor.parameters.getParameter("panAngle"), panRelay, nullptr};
+  juce::WebComboBoxParameterAttachment panModeAttachment{
+      *audioProcessor.parameters.getParameter("panRule"), panRuleRelay,
+      nullptr};
+  juce::WebToggleButtonParameterAttachment bypassAttachment{
+      *audioProcessor.parameters.getParameter("bypass"), bypassRelay, nullptr};
+
+  //==============================================================================
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(
       GainPanTutorialAudioProcessorEditor)
 };
